b58905ce633d9818a28ff610b3f801f995a28b29
Saman <ebsa491@gmail.com>
2020-03-29 12:32:43 +0430

    generate function added

--- PassGenerate.py
+++ PassGenerate.py
@@ -1,8 +1,14 @@
 
+import random
+import string
+
 class PassGenerate:
 
     def __init__(self):
         pass
+
+    def generate(self, size):
+        return ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
 
 def main():
     app = PassGenerate()
