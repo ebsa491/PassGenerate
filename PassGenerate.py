e7189a416757406c4a30e153c066c95a53bd96e2
Saman <ebsa491@gmail.com>
2020-03-29 12:40:30 +0430

    App completed

--- PassGenerate.py
+++ PassGenerate.py
@@ -3,13 +3,33 @@
 # Created at Iran (IR) .
 # Python 3 .
 
+import sys
 import random
 import string
 
 class PassGenerate:
 
     def __init__(self):
-        pass
+
+        # figlet used for ASCII
+
+        icon = open('icon.txt', 'r')
+
+        icon_contents = icon.read()
+
+        print(icon_contents)
+
+        icon.close()
+
+        size = 0
+
+        if len(sys.argv) < 2:
+            sizeInput = input("Enter the size of password: ")
+            size = int(sizeInput)
+        else:
+            size = sys.argv[1]
+
+        print(self.generate(size))
 
     def generate(self, size):
         return ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
